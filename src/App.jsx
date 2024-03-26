6e284ceda38c8aa19750fbb0b8c4db93a582e676
Gabriel Pereira <gp776945@gmail.com>
2024-03-25 23:24:42 -0300

    segundo commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import IconSedans from "./assets/icon-sedans.svg"
+import IconSuvs from "./assets/icon-suvs.svg"
+import IconsLuxury from "./assets/icon-luxury.svg"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <section>
+        <div>
+          <img src={IconSedans} alt="icone dos carros sedans" />
+          <h2>Sedans</h2>
+          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem</p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank>Ver mais</a>
+        </div>
+
+        <div>
+          <img src={IconSuvs} alt="icone dos carros Suvs" />
+          <h2>SUVs</h2>
+          <p>Ecolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road</p>
+          <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+        </div>
+
+        <div>
+          <img src={IconsLuxury} alt="icone dos carros de luxo" />
+          <h2>Luxuosos</h2>
+          <p>Passeie nos melhores carros os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank>Ver mais</a>
+        </div>
+      </section>
+    </main>
   )
 }
-
-export default App
